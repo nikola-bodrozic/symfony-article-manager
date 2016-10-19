cde7c7506350b9ecb8e21193bda1d8670e2fdd9d
Nikola Bodrozic <nikola.bodrozic@users.noreply.github.com>
2016-10-19 21:18:59 +0200

    Chp 02 - 19 handling submission form

--- src/MyCompany/UserBundle/Controller/RegisterController.php
+++ src/MyCompany/UserBundle/Controller/RegisterController.php
@@ -1,22 +1,50 @@
 <?php
 namespace MyCompany\UserBundle\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;  
+use MyCompany\UserBundle\Entity\User;
+
 class RegisterController extends Controller
 {
     /**
      * @Route("/register", name="user_register")
      * @Template()
      */
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $form = $this->createFormBuilder()
-            ->add('username', 'text')
-            ->add('email', 'text')
-            ->add('password', 'password')
+            ->add('username', 'Symfony\Component\Form\Extension\Core\Type\TextType')
+            ->add('email', 'Symfony\Component\Form\Extension\Core\Type\EmailType')
+            ->add('password', 'Symfony\Component\Form\Extension\Core\Type\RepeatedType', ["type"=>"Symfony\Component\Form\Extension\Core\Type\PasswordType"])
             ->getForm()
         ;
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setEmail($data['email']);
+            $user->setPassword($this->encodePassword($user, $data['password']));
+            $user->setRoles(['ROLE_USER']);
+            $user->setIsActive(true);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush(); 
+            
+            $url = $this->generateUrl('news_index');
+            return $this->redirect($url);                        
+        }
         return array('form' => $form->createView());
     }
+
+    private function encodePassword(User $user, $plainPassword) {
+        $encoder = $this->container->get('security.encoder_factory')
+            ->getEncoder($user);
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }    
 }
